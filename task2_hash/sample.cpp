e852c6b504036c0933427c60a71726d1502588be
Bananeq <dankilom26@gmail.com>
2024-04-02 22:47:26 +0200

    hw02: better implementation

--- task2_hash/sample.cpp
+++ task2_hash/sample.cpp
@@ -22,70 +22,63 @@ using namespace std;
 
 #endif /* __PROGTEST__ */
 
-std::string convertToHex(const std::string &input) {
-    static const char hex[] = "0123456789abcdef";
-    std::string output(input.size() * 2, '\0');
 
-    for (size_t i = 0; i < input.size(); ++i) {
-        output[i * 2] = hex[(unsigned char)input[i] >> 4];
-        output[i * 2 + 1] = hex[(unsigned char)input[i] & 15];
+bool checkZeroBits(const std::string &hash, int bits) {
+    int zeroChars = bits / 4;
+    int zeroBits = bits % 4;
+
+    for (int i = 0; i < zeroChars; ++i)
+        if (hash[i] != '0')
+            return false;
+
+    if (zeroBits > 0) {
+        char mask = (1 << (4 - zeroBits)) - 1 + '0';
+        if (hash[zeroChars] > mask)
+            return false;
     }
 
-    return output;
-}
-
-std::string generateRandomMessage(int length) {
-    std::string random_string(length, '\0');
-    RAND_bytes((unsigned char *)random_string.data(), length);
-    return random_string;
-}
-
-std::string calculateHash(const std::string &message, const EVP_MD *md) {
-    unsigned char hash[EVP_MAX_MD_SIZE];
-    unsigned int hashLen;
-
-    EVP_Digest(message.c_str(), message.size(), hash, &hashLen, md, NULL);
-
-    std::string result(hashLen * 2, '0');
-    static const char* digits = "0123456789abcdef";
-
-    for(unsigned int i = 0; i < hashLen; ++i) {
-        result[i * 2] = digits[hash[i] >> 4];
-        result[i * 2 + 1] = digits[hash[i] & 0xf];
-    }
-
-    return result;
+    return true;
 }
 
 int findHashEx(int bits, string &message, string &hash, string_view hashType) {
-    if (bits < 0) 
-        return 0;
-
+    if (bits < 0) return 0;
     const EVP_MD *md = EVP_get_digestbyname(hashType.data());
-    if (!md) 
-        return 0;
+    if (!md) return 0;
 
-    int messageLength = 64;//EVP_MD_size(md);
-    if (bits > messageLength * 8) 
-        return 0;
+    int messageLength = EVP_MD_size(md);
+    if (bits > messageLength * 8) return 0;
+    message.resize(messageLength);
+    RAND_bytes((unsigned char *)message.data(), messageLength);
+    static const char* digits = "0123456789abcdef";
     
-    message = generateRandomMessage(messageLength);
-    int zeroChars = bits >> 2;
-    int zeroBits = bits & 3;
-    std::string mask(zeroChars, '0');
-    if (zeroBits > 0) {
-        char maskChar = (1 << (4 - zeroBits)) - 1 + '0';
-        mask += maskChar;
-    }
-
     while (true) {
-        hash = calculateHash(message, md);
+        std::vector<unsigned char> hash_v(EVP_MAX_MD_SIZE);
+        unsigned int hashLen;
 
-        if (hash.compare(0, mask.size(), mask) == 0){
-            message = convertToHex(message);
+        EVP_Digest(message.c_str(), message.size(), hash_v.data(), &hashLen, md, NULL);
+        if ((unsigned int)bits > hashLen * 8) return 0;
+        std::string hash_(hashLen * 2, '0');
+
+        for(unsigned int i = 0; i < hashLen; ++i) {
+            hash_[i * 2] = digits[hash_v[i] >> 4];
+            hash_[i * 2 + 1] = digits[hash_v[i] & 0xf];
+        }
+
+        if (checkZeroBits(hash_, bits)) {
+            auto size = message.size();
+            std::string output(size * 2, '\0');
+            for (size_t i = 0; i < size; ++i) {
+                output[i * 2] = digits[(unsigned char)message[i] >> 4];
+                output[i * 2 + 1] = digits[(unsigned char)message[i] & 15];
+            }
+            std::swap(message, output);
+            std::swap(hash, hash_);
+            cout << "Message: " << message << endl;
+            cout << "Hash: " << hash << endl;
+            
             return 1;
         }
-        std::swap(message, hash);
+        std::swap(message, hash_);
     }
 }
 
@@ -93,18 +86,36 @@ int findHash(int bits, string &message, string &hash) {
     return findHashEx(bits, message, hash, "sha512");
 }
 
+
 #ifndef __PROGTEST__
 
-int checkHash(int bits, const string &hash) {
-    for (int i = 0; i < bits; i++)
-        if (hash[i] == i) 
-            return 0;
+int checkHash(int bits, const string &hexHash) {
+    size_t zeros = bits/4;
+    int remainingBits = bits % 4;
+    if (hexHash.size() < zeros + 1) return 0;
+    for (size_t i = 0; i < zeros; ++i) {
+        if (hexHash[i] != '0') return 0;
+    }
+    if (remainingBits > 0) {
+        switch(remainingBits) {
+            case 1:
+                if (hexHash[zeros] > '7') return 0;
+                break;
+            case 2:
+                if (hexHash[zeros] > '3') return 0;
+                break;
+            case 3:
+                if (hexHash[zeros] > '1') return 0;
+                break;
+        }
+    }
     return 1;
 }
 
 int main () {
     string hash, message;
     assert(findHash(0, message, hash) == 1);
+    assert(!message.empty() && !hash.empty());
     assert(!message.empty() && !hash.empty() && checkHash(0, hash));
     message.clear();
     hash.clear();
